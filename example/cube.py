25e5482f17556ed42264fd74d602b50217c71b01
philgras <thebigone@grassal-mueller.de>
2023-12-29 15:02:59 +0100

    added examples

---
+++ example/cube.py
@@ -0,0 +1,49 @@
+from pyrast.geometry.scene import Camera, DirectionalLight, Material
+from pyrast.geometry.generate import create_cube, create_checkerboard_texture
+from pyrast.render import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+mesh = create_cube()
+mesh.textures["diffuse"] = create_checkerboard_texture()
+mesh.material = Material()
+
+res = 512
+cam = Camera(res, (res, res))
+cam.look_at(mesh.location, np.pi / 3, 0.4, distance=4.0)
+
+render = Render(
+    rasterizer=Rasterizer(cam.image_size),
+    shaders=[
+        NormalShader(),
+        UVShader(),
+        TextureShader(),
+        CoordinateShader(),
+        PhongShader()
+    ]
+)
+
+light = DirectionalLight(direction=(0.3, 0., 1.), color=(1.,1.,1.))
+
+out = render(mesh, cam, light)
+
+# collect outputs
+mask_img = ~out["mask_img"]
+# normal image
+normal_img = out["normal_img"] * 0.5 + 0.5
+normal_img[mask_img] = 0
+# uv image
+uv_img = out["uv_img"] * 0.5 + 0.5
+uv_img[mask_img] = 0
+uv_img = np.concatenate([uv_img, np.zeros_like(uv_img[..., [0]])], axis=-1)
+# depth image
+depth_img = out["z_buffer"]
+shaded_img = out["shaded_img"]
+outputs = [normal_img, uv_img, depth_img, shaded_img]
+
+# visualize
+_, axes = plt.subplots(1, 4, figsize=(16, 4))
+for out, ax in zip(outputs, axes):
+    ax.imshow(out)
+plt.show()
---
+++ example/mesh_from_obj.py
@@ -0,0 +1,55 @@
+from pyrast.geometry.scene import Camera, DirectionalLight, Material
+from pyrast.render import *
+from pyrast.util.io import load_obj
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# load mesh and set material
+mesh = load_obj("./example/suzanne.obj")
+material = Material.Pearl()
+mesh.material = material
+
+# setup camera
+res = 512
+cam = Camera(res, (res, res))
+cam.look_at(mesh.location, np.pi / 3, 0.4, distance=4.0)
+
+
+# setup light
+light = DirectionalLight(direction=(0.3, 0., 1.), color=(1., 1., 1.))
+
+# define render
+render = Render(
+    rasterizer=Rasterizer(cam.image_size),
+    shaders=[
+        NormalShader(),
+        UVShader(),
+        TextureShader(),
+        CoordinateShader(),
+        PhongShader()
+    ]
+)
+
+out = render(mesh, cam, light)
+
+# collect outputs
+mask_img = ~out["mask_img"]
+# normal image
+normal_img = out["normal_img"] * 0.5 + 0.5
+normal_img[mask_img] = 0
+# uv image
+uv_img = out["uv_img"]# * 0.5 + 0.5
+uv_img[mask_img] = 0
+uv_img = np.concatenate([uv_img, np.zeros_like(uv_img[..., [0]])], axis=-1)
+# depth image
+depth_img = out["z_buffer"]
+shaded_img = out["shaded_img"]
+outputs = [normal_img, uv_img, depth_img, shaded_img]
+
+# visualize
+_, axes = plt.subplots(1, 4, figsize=(16, 4))
+for out, ax in zip(outputs, axes):
+    ax.imshow(out)
+plt.show()
